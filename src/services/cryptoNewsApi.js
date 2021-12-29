5c2c0bd6afd2d69bcfe14acd6640ccb6a496685a
mamadou SY <mamadousy1254@gmail.com>
2021-12-29 17:41:01 +0000

    Update cryptoNewsApi.js

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,11 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const cryptoNewsHeaders = {
-  "x-bingapis-sdk": "true",
-  "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
-  "x-rapidapi-key": "b026c96035msh3a2baeae79989f9p17ccfejsn56c652a6dd97",
-};
-
 const baseUrl = "https://bing-news-search1.p.rapidapi.com";
 
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
