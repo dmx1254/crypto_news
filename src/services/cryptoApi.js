8ca119cabc2ba77b654aa6b1a28c866d6247f106
mamadou SY <mamadousy1254@gmail.com>
2021-12-29 17:38:17 +0000

    Update cryptoApi.js

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,19 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const cryptoApiHeaders = {
-  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
-  "x-rapidapi-key": "b026c96035msh3a2baeae79989f9p17ccfejsn56c652a6dd97",
-};
+
 
 const baseUrl = "https://coinranking1.p.rapidapi.com";
 
-// var options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/stats',
-//     headers: {
-//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//       'x-rapidapi-key': 'b026c96035msh3a2baeae79989f9p17ccfejsn56c652a6dd97'
-//     }
-//   };
+
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
